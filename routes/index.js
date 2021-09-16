161d190ec5a746459220129199c1952aa57d85c3
karla-walker <indiablonix@gmail.com>
2021-09-16 01:02:23 -0700

    Created routes file and added psudo code for routes

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// routes
+
+// GET user
+// GET goals related to user
+// GET journal entries related to user
+
+// POST new user
+// POST new goal
+// POST new journal entry
+
+// PUT goal
+// PUT journal entry
+
+// DELETE goal
+// DELETE journal entry
+
+module.exports = router;
