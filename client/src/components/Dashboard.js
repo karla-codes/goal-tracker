3302c5f706aabaa18f2df492e99336f69014106b
karla-walker <indiablonix@gmail.com>
2021-09-29 13:09:21 -0700

    Initial commit

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <main>
+      <header>
+        <div>
+          <h1>13-Week Goals</h1>
+          <p>
+            Select a goal to view/edit goal details and a dedicated journal to
+            document your progress.
+          </p>
+        </div>
+        <a href="">+ Create New Goal</a>
+      </header>
+      {/* Add goals here */}
+    </main>
+  );
+}
+
+export default Dashboard;
