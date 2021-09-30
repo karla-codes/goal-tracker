f6e9f31753d623201f7670b52db127bb4775a2df
karla-walker <indiablonix@gmail.com>
2021-09-29 18:41:26 -0700

    Initial commit

---
+++ client/src/components/JournalPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function JournalPage() {
+  return (
+    <div className="journal-page">
+      <p>
+        This is where the first chunk of the journal page will be displayed.
+      </p>
+      <span>September 29, 2021</span>
+    </div>
+  );
+}
+
+export default JournalPage;
