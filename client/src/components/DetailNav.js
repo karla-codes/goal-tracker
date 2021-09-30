e0a359a22ed3e6abdb38ec5cc93abc7ad75bfa56
karla-walker <indiablonix@gmail.com>
2021-09-29 18:47:59 -0700

    Initial commit

---
+++ client/src/components/DetailNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DetailNav() {
+  return (
+    <div className="detail-nav">
+      <a href="">Back to Goals</a>
+      <div>
+        <a href="">Edit Goal Details</a>
+        <a href="">Delete Goal</a>
+      </div>
+    </div>
+  );
+}
+
+export default DetailNav;
