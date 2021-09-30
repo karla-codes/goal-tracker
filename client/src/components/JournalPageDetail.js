39128fb90f5447f11067e0bf1f692b09cf32fc73
karla-walker <indiablonix@gmail.com>
2021-09-29 18:54:17 -0700

    Initial commit

---
+++ client/src/components/JournalPageDetail.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import DetailNav from './DetailNav';
+
+function JournalPageDetail() {
+  return (
+    <main>
+      <DetailNav />
+      <div className="journal-page-detail">
+        <h1>September 29, 2021</h1>
+        <p>
+          Candy candy jelly beans chupa chups gummi bears cookie. Donut biscuit
+          dragée wafer chocolate bar gingerbread jelly-o cupcake carrot cake.
+          Chocolate cake chocolate cake cheesecake toffee apple pie cotton candy
+          dessert. Carrot cake oat cake sugar plum caramels tootsie roll candy
+          canes muffin topping I love. Chocolate bar biscuit gingerbread oat
+          cake wafer croissant apple pie shortbread chocolate. Topping sweet
+          roll croissant I love caramels chocolate bar. Soufflé pastry I love
+          dragée carrot cake. I love sweet roll cheesecake carrot cake I love
+          croissant brownie macaroon muffin. Liquorice biscuit I love caramels
+          gummi bears bonbon. Jelly donut pie sweet roll caramels. Soufflé gummi
+          bears carrot cake lollipop icing chocolate wafer. Pastry ice cream
+          jelly beans toffee brownie I love cake. Soufflé jelly jujubes tart oat
+          cake. Cake lemon drops candy jujubes cupcake. Tart carrot cake cake I
+          love muffin oat cake. Cotton candy croissant macaroon chocolate jelly
+          beans jelly croissant I love. Candy canes cotton candy powder cupcake
+          cotton candy gingerbread. Ice cream marshmallow I love icing chocolate
+          cake pastry I love. Tiramisu shortbread chocolate cake sweet toffee
+          bonbon.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default JournalPageDetail;
