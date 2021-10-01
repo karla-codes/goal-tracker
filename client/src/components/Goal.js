800d97c0cc9ff6daf11334c84cade9701ff7135b
karla-walker <indiablonix@gmail.com>
2021-09-30 22:45:46 -0700

    Changed span tag to p tag

--- client/src/components/Goal.js
+++ client/src/components/Goal.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Goal() {
   return (
-    <div>
-      <h2>The goal goes here</h2>
-      <span>Tag</span>
+    <div className="goal">
+      <h2>This is an example of a goal someone might have.</h2>
+      <p className="goal-category">Category</p>
     </div>
   );
 }
