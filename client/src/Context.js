70f1a77f02113b5f42412bac8c189192b7607bae
karla-walker <indiablonix@gmail.com>
2021-10-10 21:19:52 -0700

    Initial commit

---
+++ client/src/Context.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Context = React.createContext();
+
+export const Provider = props => {
+  // data that needs to be accessed by multiple components
+  // - Authenticated user
+  // - api request helper
+};
---
+++ client/src/Data.js
@@ -0,0 +1,65 @@
+// this is the api request helper
+
+import { options } from "../../api/routes";
+
+function Data() {
+  function api(
+    path,
+    method,
+    body = null,
+    requiresAuth = false,
+    credentials = null
+  ) {
+    const url = "http://localhost:5000/api" + path;
+
+    // api request config
+    const options = {
+      method,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    };
+
+    if (body !== null) {
+      options.body = JSON.stringify(body);
+    }
+
+    // check if needs authorization
+    if (requiresAuth) {
+      const encodedCredentials = btoa(
+        `${credentials.email}:${credentials.password}`
+      );
+
+      options.headers["Authorization"] = `Basic ${encodedCredentials}`;
+    }
+
+    return fetch(path, options);
+  }
+
+  // API requests
+
+  // GET user
+  async function getUser(email, password) {
+    const user = await api("/users", "GET", null, true, { email, password });
+
+    if (user.status === 200) {
+      return user.json().then(data => data);
+    } else if (user.status === 401) {
+      return null;
+    }
+  }
+
+  // POST (create) user
+
+  // GET user goals
+
+  // POST (create) goal
+
+  // DELETE goal
+
+  // GET goal detail
+
+  // PUT (edit) goal detail
+}
+
+export default Data;
