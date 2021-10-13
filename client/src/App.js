c35c249700918399aaf319491750619eb74a8c8b
karla-walker <indiablonix@gmail.com>
2021-10-12 23:54:02 -0700

    Import withContext

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,30 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './style.css';
 
+import withContext from './Context';
+
 import Navigation from './components/Navigation';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
 import Dashboard from './components/Dashboard';
 import GoalDetail from './components/GoalDetail';
 import JournalPageDetail from './components/JournalPageDetail';
 import EditGoal from './components/EditGoal';
 import CreateGoal from './components/CreateGoal';
 
+const UserSignInWithContext = withContext(UserSignIn);
+const NavigationWithContext = withContext(Navigation);
+const UserSignOutWithContext = withContext(UserSignOut);
+
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
+      <NavigationWithContext />
       <Switch>
-        <Route exact path="/" component={UserSignIn} />
+        <Route exact path="/" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUp} />
+        <Route path="/signout" component={UserSignOutWithContext} />
         <Route path="/goals/new" component={CreateGoal} />
         <Route
           path="/goals/:id/journal/:pageId"
