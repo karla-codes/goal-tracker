940bb8a0689bb3aa9c878d0839d19f3b04d3a5f1
karla-walker <indiablonix@gmail.com>
2021-10-14 01:10:25 -0700

    Add components with Context

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,22 @@ import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
 import Dashboard from './components/Dashboard';
 import GoalDetail from './components/GoalDetail';
-import JournalPageDetail from './components/JournalPageDetail';
+// import JournalPageDetail from './components/JournalPageDetail';
 import EditGoal from './components/EditGoal';
 import CreateGoal from './components/CreateGoal';
+import DeleteGoal from './components/DeleteGoal';
+import NotFound from './components/NotFound';
+import Errors from './components/Errors';
 
 const UserSignInWithContext = withContext(UserSignIn);
 const NavigationWithContext = withContext(Navigation);
 const UserSignOutWithContext = withContext(UserSignOut);
+const DashboardWithContext = withContext(Dashboard);
+const GoalDetailWithContext = withContext(GoalDetail);
+const UserSignUpWithContext = withContext(UserSignUp);
+const DeleteGoalWithContext = withContext(DeleteGoal);
+const CreateGoalWithContext = withContext(CreateGoal);
+const EditGoalWithContext = withContext(EditGoal);
 
 function App() {
   return (
@@ -24,16 +33,20 @@ function App() {
       <NavigationWithContext />
       <Switch>
         <Route exact path="/" component={UserSignInWithContext} />
-        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
-        <Route path="/goals/new" component={CreateGoal} />
-        <Route
+        <Route path="/goals/new" component={CreateGoalWithContext} />
+        {/* <Route
           path="/goals/:id/journal/:pageId"
           component={JournalPageDetail}
-        />
-        <Route path="/goals/:id/edit" component={EditGoal} />
-        <Route path="/goals/:id" component={GoalDetail} />
-        <Route path="/goals" component={Dashboard} />
+        /> */}
+        <Route path="/goals/:id/delete" component={DeleteGoalWithContext} />
+        <Route path="/goals/:id/edit" component={EditGoalWithContext} />
+        <Route path="/goals/:id" component={GoalDetailWithContext} />
+        <Route path="/goals" component={DashboardWithContext} />
+        <Route path="/errors" component={Errors} />
+        <Route path="/notfound" component={NotFound} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
