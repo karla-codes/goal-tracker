04d17bca29fb2ae70c918ce1e5344308b62db3ac
karla-walker <indiablonix@gmail.com>
2021-10-14 01:04:07 -0700

    Add state and fetch goal details function

--- client/src/components/GoalDetail.js
+++ client/src/components/GoalDetail.js
@@ -1,33 +1,63 @@
-import React from 'react';
-import JournalPage from './JournalPage';
+import React, { useEffect, useState } from 'react';
+// import JournalPage from './JournalPage';
 import DetailNav from './DetailNav';
+import { useParams } from 'react-router';
 
-function GoalDetail() {
+function GoalDetail(props) {
+  const { context } = props;
+  const { data, authUser } = context;
+  const { id } = useParams();
+  const [goalDetails, setGoalDetails] = useState({});
+
+  useEffect(() => {
+    const fetchGoals = async () => {
+      const goalDetails = await data
+        .getGoal(id, authUser)
+        .then(data => data)
+        .catch(err => {
+          console.log(err);
+          props.history.push('/errors');
+        });
+
+      console.log(authUser);
+      console.log(goalDetails);
+
+      if (goalDetails.message) {
+        console.log(goalDetails.message);
+        props.history.push('/notfound');
+      } else {
+        setGoalDetails(goalDetails);
+      }
+    };
+
+    fetchGoals();
+  }, []);
+
   return (
     <main>
-      <DetailNav />
+      <DetailNav goalId={id} />
       <div className="goal-details">
         <div className="goal-text">
           <h1>Goal</h1>
-          <p>This is where the goal goes</p>
-          <span>Tag</span>
+          <p>{goalDetails.goal}</p>
+          <span>{goalDetails.category}</span>
         </div>
         <div className="details-container">
           <div className="detail">
             <h2>Motivations</h2>
-            <p>Enter user filled text</p>
+            <p>{goalDetails.motivations}</p>
           </div>
           <div className="detail">
             <h2>Progress Milestones</h2>
-            <p>Enter user filled text</p>
+            <p>{goalDetails.progressMilestones}</p>
           </div>
           <div className="detail">
             <h2>Accountability</h2>
-            <p>Enter user filled text</p>
+            <p>{goalDetails.accountability}</p>
           </div>
         </div>
       </div>
-      <div className="goal-journal">
+      {/* <div className="goal-journal">
         <div className="goal-nav">
           <h2>Journal</h2>
           <a href="">Create New Page</a>
@@ -35,7 +65,7 @@ function GoalDetail() {
         <div className="journal-pages">
           <JournalPage />
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
