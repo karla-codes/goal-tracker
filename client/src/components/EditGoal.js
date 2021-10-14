5b2251553e16b4089c0db364264c0153df515ee1
karla-walker <indiablonix@gmail.com>
2021-10-14 01:05:39 -0700

    Pass context to GoalForm

--- client/src/components/EditGoal.js
+++ client/src/components/EditGoal.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import GoalForm from './GoalForm';
 
-function EditGoal() {
-  return <GoalForm headerText="Edit Goal" />;
+function EditGoal(props) {
+  const { context } = props;
+  const { id } = useParams();
+
+  return <GoalForm headerText="Edit Goal" context={context} id={id} />;
 }
 
 export default EditGoal;
