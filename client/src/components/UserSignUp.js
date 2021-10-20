4339da23175d1b68d7842fe5deec80e496e3112a
karla-walker <indiablonix@gmail.com>
2021-10-19 21:11:50 -0700

    Add user signup form functionality

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 
 import Form from './Form';
 
-function UserSignUp() {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function UserSignUp(props) {
+  const [formValues, setFormValues] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const { context } = props;
 
   // track state for the following:
   // - form values (email, password, fullName)
   // - form errors
-  //
 
   // Add form validation
   // Check if:
@@ -19,28 +23,108 @@ function UserSignUp() {
   // 2. email input is empty
   // 3. password input is empty
   // 4. password has min 8 characters (regex)
+  function formValidation(values) {
+    let errors = {};
+    const regex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+
+    if (!values.fullName) {
+      errors.fullName = 'Cannot leave name blank';
+    }
+
+    if (!values.email) {
+      errors.email = 'Cannot leave email blank';
+    } else if (!regex.test(values.email)) {
+      errors.email = 'Invalid email format';
+    }
+
+    if (!values.password) {
+      errors.password = 'Cannot leave password blank';
+    } else if (values.password.length < 8) {
+      errors.password = 'Password needs to be more than 8 characters long';
+    }
+
+    return errors;
+  }
+
+  function handleChange(e) {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { from } = props.history.state || { from: '/goals' };
+    setErrors(formValidation(formValues));
+
+    if (errors.fullName || errors.email || errors.password) {
+      return;
+    } else {
+      // send user sign up request
+      context.data
+        .createUser(formValues)
+        .then(errors => {
+          if (errors) {
+            setErrors({ email: errors });
+          } else {
+            console.log(
+              `${formValues.fullName} is successfully signed up and authenticated!`
+            );
+            context.actions.signIn(formValues).then(user => {
+              console.log(user);
+              if (user === null) {
+                setErrors({ fetch: 'Sign in was unsuccessfull' });
+              } else {
+                props.history.push(from);
+              }
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          props.history.push('/errors');
+        });
+    }
+  }
 
   return (
     <main>
       <div className="form-wrapper">
         <h1>Sign Up</h1>
         <Form
-          submit={submit}
-          cancel={cancel}
+          submit={handleSubmit}
           buttonText="Sign Up"
           elements={() => (
             <>
               <label htmlFor="fullName">Full Name:</label>
-              <input name="fullName" id="fullName" type="text"></input>
+              <input
+                name="fullName"
+                id="fullName"
+                type="text"
+                onChange={handleChange}
+              ></input>
+              {errors.fullName && <span>{errors.fullName}</span>}
               <label htmlFor="email">Email:</label>
-              <input name="email" id="email" type="email"></input>
+              <input
+                name="email"
+                id="email"
+                type="email"
+                onChange={handleChange}
+              ></input>
+              {errors.email && <span>{errors.email}</span>}
               <label htmlFor="password">Password:</label>
-              <input name="password" id="password" type="password"></input>
+              <input
+                name="password"
+                id="password"
+                type="password"
+                onChange={handleChange}
+              ></input>
+              {errors.password && <span>{errors.password}</span>}
             </>
           )}
         />
         <p>
-          Already have an account?Login <Link to="/">here</Link>!
+          Already have an account? Login <Link to="/">here</Link>!
         </p>
       </div>
     </main>
