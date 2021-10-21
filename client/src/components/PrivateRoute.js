f1215ba74f91461c1fb2ae69951e7b16b7f5e567
karla-walker <indiablonix@gmail.com>
2021-10-20 18:54:34 -0700

    Reverse changes

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { Consumer } from '../Context';
 
-function PrivateRoute(props) {
-  const { context } = props;
-  const { authUser } = context;
-
-  return authUser ? <>{props.children} </> : <Redirect to="/" />;
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Consumer>
+      {context => (
+        <Route
+          {...rest}
+          render={props =>
+            context.authUser ? (
+              <Component {...props} />
+            ) : (
+              <Redirect
+                to={{ pathname: '/', state: { from: props.location } }}
+              />
+            )
+          }
+        />
+      )}
+    </Consumer>
+  );
 }
-
-// function PrivateRoute({ component: Component, ...rest }) {
-//   return (
-//     <Consumer>
-//       {context => (
-//         <Route
-//           {...rest}
-//           render={props =>
-//             context.authUser ? (
-//               <Component {...props} />
-//             ) : (
-//               <Redirect
-//                 to={{ pathname: '/', state: { from: props.location } }}
-//               />
-//             )
-//           }
-//         />
-//       )}
-//     </Consumer>
-//   );
-// }
 
 export default PrivateRoute;
