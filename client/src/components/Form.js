06b7dba620875918c092138bb763f89474b33804
karla-walker <indiablonix@gmail.com>
2023-01-28 16:44:57 -0800

    Add modal

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
+import Modal from "./Modal"
 
 function Form(props) {
-  const { elements, submit, buttonText } = props;
+  const { elements, submit, buttonText } = props
+  const [modal, setModal] = useState("")
+
+  useEffect(() => {
+    const modalOuter = document.querySelector(".modal-outer")
+    setModal(modalOuter)
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // setClicks(clicks + 1)
+    modal.classList.remove("open")
+  }
 
   return (
-    <form onSubmit={submit}>
-      {elements()}
-      <button
-        className="button"
-        type="submit"
-        value={buttonText}
-        name={buttonText}
-      >
-        {buttonText}
-      </button>
-    </form>
-  );
+    <>
+      {document.location.pathname === "/" ? <Modal handleSubmit={handleSubmit} /> : ""}
+
+      <form onSubmit={submit}>
+        {elements()}
+        <button className="button" type="submit" value={buttonText} name={buttonText}>
+          {buttonText}
+        </button>
+      </form>
+    </>
+  )
 }
 
-export default Form;
+export default Form
